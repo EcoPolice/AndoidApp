19179b4ab131455725fc36e847e1448e801addd6
yoloroy <slart2108@gmail.com>
2021-04-25 09:54:27 +0300

    :ambulance: fix model

--- app/src/main/java/com/yoloroy/disastersMonitor/models/Disaster.kt
+++ app/src/main/java/com/yoloroy/disastersMonitor/models/Disaster.kt
@@ -6,7 +6,8 @@ data class Disaster(
     @SerializedName("id") val id: Int,
     @SerializedName("name") val name: String,
     @SerializedName("images") val images: List<String>,
-    @SerializedName("coordinates") val coordinates: Pair<Double, Double>,
+    @SerializedName("lat") val lat: Double,
+    @SerializedName("long") val lng: Double,
     @SerializedName("date") val date: Int,
     @SerializedName("description") val description: String,
     @SerializedName("objectName") val objectName: String,
@@ -17,4 +18,39 @@ data class Disaster(
     @SerializedName("area") val area: Int, // square meters
     @SerializedName("damagedCount") val damagedCount: Int,
     @SerializedName("damagedObjects") val damagedObjects: List<String>
-)
+) {
+    constructor(
+        id: Int,
+        name: String,
+        images: List<String>,
+        coords: Pair<Double, Double>,
+        date: Int,
+        description: String,
+        objectName: String,
+        owner: String,
+        cause: String,
+        product: String,
+        volume: String,
+        area: Int,
+        damagedCount: Int,
+        damagedObjects: List<String>
+    ) : this(
+        id,
+        name,
+        images,
+        coords.first,
+        coords.second,
+        date,
+        description,
+        objectName,
+        owner,
+        cause,
+        product,
+        volume,
+        area,
+        damagedCount,
+        damagedObjects
+    )
+
+    val coordinates: Pair<Double, Double> get() = lat to lng
+}
